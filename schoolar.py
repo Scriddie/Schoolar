844d9449b8ecd3e55f97f3a28d99c6ee21a47722
Scriddie <alexander.reisach@gmail.com>
2022-03-04 15:44:26 +0100

    no session for now

--- schoolar.py
+++ schoolar.py
@@ -7,9 +7,9 @@ from SchoolarFlask.query import *
 
 # generate app and start session
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 
 @app.route("/")
